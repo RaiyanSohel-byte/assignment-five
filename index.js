8e9901ccccef5065491ed6771035477e95cc4a09
RaiyanSohel-byte <afnan19262@gmail.com>
2025-08-28 00:02:54 +0600

    coin count functionality added

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const heartIcon = document.querySelectorAll(`.heart-icon`);
 const heartCount = document.getElementById("heart-count");
-let count = 0;
+const callButton = document.querySelectorAll(`.call-button`);
+const coin = document.getElementById("coin-count");
+let countHeart = 0;
+let countCoin = 100;
+//clicking heart icon
 heartIcon.forEach((icon) => {
   icon.addEventListener("click", () => {
-    count++;
-    heartCount.innerText = count;
+    countHeart++;
+    heartCount.innerText = countHeart;
+  });
+});
+
+//clicking call button
+callButton.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    countCoin = countCoin - 20;
+    coin.innerText = countCoin;
+    if (countCoin < 20) {
+      alert("Not enough coin");
+    }
   });
 });
